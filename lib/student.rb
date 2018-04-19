a8eb8136905197dc10c084e9ffdb56c6073aa613
Sean Febo <seanfebo@techie.com>
2018-04-19 18:55:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,6 +6,14 @@ class Student
 
   def initialize(student_hash)
     @name = studen_hash[:name]
+    @location = studen_hash[:location]
+    @twitter = studen_hash[:twitter]
+    @linkedin = studen_hash[:linkedin]
+    @gitub = studen_hash[:github]
+    @blog = studen_hash[:blog]
+    @profile_quote = studen_hash[:profile_quote]
+    @bio = studen_hash[:bio]
+    @profile_url = studen_hash[:profile_url]
   end
     
   
