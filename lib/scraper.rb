2c8229adbb3e7fb21afdc2aafe44285e265586e7
Sean Febo <seanfebo@techie.com>
2018-04-19 18:38:27 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,8 +17,22 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    new = {}
     doc = Nokogiri::HTML(open(profile_url))
-    
+    new[:profile_quote] = doc.css(".")
+    doc.css(".social-icon-container a").each do |link|
+     url = anchor.attribute("href").text
+      if url =~ /twitter/
+        new[:twitter] = url
+      elsif url =~ /linkedin/
+        new[:linkedin] = url
+      elsif url =~ /github/
+        new[:github] = url
+      else
+        new[:blog]=url
+      end
+    end
+    new
   end
 
 end
